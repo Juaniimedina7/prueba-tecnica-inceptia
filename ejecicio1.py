c3c941b5e4ffdc1ec03a207d2578387a27ecdb9b
Juaniimedina7 <juanignaciomedina02@gmail.com>
2024-02-23 09:41:12 -0300

    excercise 1 modified

--- ejecicio1.py
+++ ejecicio1.py
@@ -18,7 +18,6 @@ class GeoAPI:
     API_KEY = "d81015613923e3e435231f2740d5610b"
     LAT = "-35.836948753554054"
     LON = "-61.870523905384076"
-    WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather"
 
     @classmethod
     def is_hot_in_pehuajo(cls):
